f4032b4fb74f73c5a6707cd019fc12500ed2b8ef
NikoKVCS <kvcsgame@hotmail.com>
2019-06-24 15:33:51 +0800

    s

--- mysql.go
+++ mysql.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func mysql_connect() *DB {
+
+	// root:123456@tcp(127.0.0.1:3306)/mysql?charset=utf8
+	// root:Mysql的密码@tcp(ip地址与端口)/数据库名称?charset=utf8
+	db, err := sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/mysql?charset=utf8")
+	if err != nil {
+		print("f")
+		panic(err)
+	}
+	rows, _ := db.Query("desc user_info")
+	defer rows.Close()
+	dbNames := make([]string, 0)
+	for rows.Next() {
+		var name string
+		if err = rows.Scan(&name); err != nil {
+			return
+		}
+		dbNames = append(dbNames, name)
+	}
+	fmt.Print(dbNames)
+
+	stmt, _ := db.Prepare("update user_info set username=? where id=?")
+	res, _ := stmt.Exec("lisi", 1)
+	affect, _ := res.RowsAffected()
+	fmt.Print(affect)
+	//defer db.Close()
+	return db
+}
--- router.go
+++ router.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+func getSearchUser(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
