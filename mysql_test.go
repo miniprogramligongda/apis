c375e7ea488c366eb7c01261c78ad86436bdb4be
NikoKVCS <kvcsgame@hotmail.com>
2019-06-25 10:11:43 +0800

    pass all test

--- mysql_test.go
+++ mysql_test.go
@@ -1,22 +1,63 @@
 package main
 
 import (
-	"crypto/md5"
+	"fmt"
+	"math/rand"
 	"testing"
 	"time"
 )
+
+func RandInt(min, max int) int {
+	rand.Seed(time.Now().UnixNano())
+	if min >= max || min == 0 || max == 0 {
+		return max
+	}
+	return rand.Intn(max-min) + min
+}
 
 func TestMysql(t *testing.T) {
 	db := MysqlConstruct()
 	defer db.Disconnect()
 
-	timeStr := time.Now().Format("2006-01-02 15:04:05")
+	id := RandInt(5, 1000)
+	username := time.Now().Format("2006-01-02 15:04:05")
+	password := time.Now().Format("2006-01-02")
 
-	Md5Inst := md5.New()
-	Md5Inst.Write([]byte(timeStr))
-	username := Md5Inst.Sum([]byte(""))
+	db.Insert("INSERT user_info SET id=?,username=?,password=?", id, username, password)
+	fmt.Printf("Insert test : %d %s %s\n", id, username, password)
 
-	username :=
-		db.Insert("INSERT user_info SET id=?,username=?,password=?")
-	t.Errorf("uppercase(%s) = %s,must be %s", ut.in, uc, ut.out)
+	rows := db.Query("SELECT * FROM user_info WHERE id=?", id)
+	for rows.Next() {
+		var _id int
+		var _username string
+		var _password string
+		err := rows.Scan(&_id, &_username, &_password)
+		checkErr(err)
+		fmt.Printf("Query test : %d %s %s\n", _id, _username, _password)
+	}
+
+	update_username := "nikokvcs"
+	db.Update("update user_info set username=? where id=?", update_username, id)
+	fmt.Printf("Update test : %d %s\n", id, update_username)
+
+	rows = db.Query("SELECT * FROM user_info WHERE id=?", id)
+	for rows.Next() {
+		var _id int
+		var _username string
+		var _password string
+		err := rows.Scan(&_id, &_username, &_password)
+		checkErr(err)
+		fmt.Printf("Query test : %d %s %s\n", _id, _username, _password)
+		if _username != update_username {
+			t.Errorf("Username updated failed")
+		}
+	}
+
+	fmt.Printf("Delete test : %d\n", id)
+	db.Delete("delete from user_info where id=?", id)
+	rows = db.Query("SELECT * FROM user_info WHERE id=?", id)
+	if rows.Next() {
+		t.Errorf("Deleted failed")
+	}
+
 }
