445641c27cba3c4b11f58a99352ebfd10a7243b0
NikoKVCS <kvcsgame@hotmail.com>
2019-06-25 09:23:17 +0800

    test

--- mysql_test.go
+++ mysql_test.go
@@ -1,8 +1,22 @@
 package main
 
-import "testing"
+import (
+	"crypto/md5"
+	"testing"
+	"time"
+)
 
 func TestMysql(t *testing.T) {
+	db := MysqlConstruct()
+	defer db.Disconnect()
 
+	timeStr := time.Now().Format("2006-01-02 15:04:05")
+
+	Md5Inst := md5.New()
+	Md5Inst.Write([]byte(timeStr))
+	username := Md5Inst.Sum([]byte(""))
+
+	username :=
+		db.Insert("INSERT user_info SET id=?,username=?,password=?")
 	t.Errorf("uppercase(%s) = %s,must be %s", ut.in, uc, ut.out)
 }
