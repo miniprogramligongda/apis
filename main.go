0cdc7cc3f5a5753f0acac303913d04c981d2a8f0
NikoKVCS <kvcsgame@hotmail.com>
2019-06-26 15:31:04 +0800

    get Idea

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"time"
+
 	"./conf"
 	"github.com/labstack/echo"
 )
 
 func main() {
 	e := echo.New()
+	e.Server.WriteTimeout = 20 * time.Minute
+	e.Server.ReadTimeout = 20 * time.Minute
 
 	//e.GET("searchuser", getSearchUser)
 	setRouter(e)
--- routers/getIdea.go
+++ routers/getIdea.go
@@ -3,13 +3,53 @@ package routers
 import (
 	"net/http"
 	"strconv"
+	"time"
 
 	"../dao"
 	"github.com/labstack/echo"
 )
 
-func checkFriends(openid string, list []*dao.Idea) {
+type jsonType struct {
+	Iid       int64
+	Openid    string
+	Time      time.Time
+	Content   string
+	Like      int64
+	Favorite  int64
+	AvatarUrl string
+	Gender    int
+	Nickname  string
+	Friend    int
+}
 
+func convertType(list []*dao.Idea, Openid string) []*jsonType {
+	result := make([]*jsonType, 0)
+	for _, item := range list {
+		u := &jsonType{}
+		u.Iid = item.Iid
+		u.Openid = item.Openid
+		u.Time = item.Time
+		u.Content = item.Content
+		u.Like = item.Like
+		u.Favorite = item.Favorite
+
+		d := dao.NewDaoUserInfo()
+		defer d.Close()
+		user, err := d.FindByOpenid(u.Openid)
+		if err == nil && user != nil {
+			u.AvatarUrl = user.AvatarUrl
+			u.Gender = user.Gender
+			u.Nickname = user.Nickname
+		}
+
+		checkFriends(u, Openid)
+		result = append(result, u)
+	}
+	return result
+}
+
+func checkFriends(u *jsonType, Openid string) {
+	u.Friend = 0
 }
 
 func getIdea(c echo.Context) error {
@@ -20,7 +60,8 @@ func getIdea(c echo.Context) error {
 		return c.JSON(http.StatusOK, "error : Page should be number")
 	}
 	d := dao.NewDaoIdea()
+	defer d.Close()
 	list, err := d.QueryPage(page)
-	checkFriends(Openid, list)
-	return c.JSON(http.StatusOK, list)
+	json := convertType(list, Openid)
+	return c.JSON(http.StatusOK, json)
 }
