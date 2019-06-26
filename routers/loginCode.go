afe26753bda375b798b5fd9e3588c627f5680ae8
ChenSmallX <641751205@qq.com>
2019-06-26 13:40:19 +0800

    wx mini prog login to call

--- routers/loginCode.go
+++ routers/loginCode.go
@@ -9,26 +9,13 @@ import (
 	"net/http"
 )
 
-// @appID 小程序 appID
-// @secret 小程序的 app secret
-// @code 小程序登录时获取的 code
-// res, err := weapp.Login(appID, secret, code)
-// if err != nil {
-//     // handle error
-// }
-
-// // res.OpenID
-// // res.SessionKey
-// // res.UnionID
-// fmt.Printf("返回结果: %#v", res)
-
 func loginCode(c echo.Context) error {
 	// from Configuration of this project
 	appID := conf.Conf.AppID
 	secret := conf.Conf.Secret
 
 	code := c.QueryParam("code")
-	avatarUrl := c.QueryParam("avatarUrl")
+	avatarURL := c.QueryParam("avatarUrl")
 	nickname := c.QueryParam("nickname")
 	gender := c.QueryParam("gender")
 
@@ -39,9 +26,11 @@ func loginCode(c echo.Context) error {
 	}
 
 	openID := res.OpenID
-
+	addUserInfo(appID, avatarURL, gender, nickname)
+	return err
 }
 
-func addUserInfo(openID, avatarUrl, gender, nickname string) error {
-	dao.Insert
+func addUserInfo(openID, avatarURL, gender, nickname string) error {
+	dao.FindByOpenid
+	return nil
 }
