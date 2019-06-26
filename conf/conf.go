a12d17e2e4b958548646b202821622c0e4cae925
ChenSmallX <641751205@qq.com>
2019-06-26 11:22:51 +0800

    modify the configuration

--- conf/conf.go
+++ conf/conf.go
@@ -2,10 +2,15 @@ package conf
 
 type Config struct {
 	DbAddr string
+	AppID  string
+	Secret string
 }
 
-var Conf = &Config{""}
+// Conf contains the basis infomation of project
+var Conf = &Config{"", "", ""}
 
 func init() {
 	Conf.DbAddr = "apis:apisapis@tcp(apis.2.chensmallx.top:3306)/APIS_DB?charset=utf8&parseTime=true"
+	Conf.AppID = "wx67763b6060ba9528"
+	Conf.Secret = ""
 }
