73f258a0296fcceab10e5fba17631b7e6d198a24
ChenSmallX <641751205@qq.com>
2019-06-26 16:35:11 +0800

    add app secret

--- conf/conf.go
+++ conf/conf.go
@@ -14,5 +14,5 @@ var Conf = &Config{"", "", "", "./conf/2412563_apis.1.chensmallx.top.pem", "./co
 func init() {
 	Conf.DbAddr = "apis:apisapis@tcp(apis.2.chensmallx.top:3306)/APIS_DB?charset=utf8&parseTime=true"
 	Conf.AppID = "wx7f5430dfe80d0c10"
-	Conf.Secret = ""
+	Conf.Secret = "1f49e3c660e1fad1bfad26ed016656d8"
 }
