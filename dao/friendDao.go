d0048592b19bb1f93e4a45c1cadb487b4dc7df25
ChenSmallX <641751205@qq.com>
2019-06-27 12:21:54 +0800

    DAO of friend: add FindbyS

--- dao/friendDao.go
+++ dao/friendDao.go
@@ -62,6 +62,13 @@ func (this *DaoFriend) FindByObjectAndStatus(objectOpenid string, status int) ([
 	return subjectList, err
 }
 
+// FindBySubjectAndStatus function
+func (this *DaoFriend) FindBySubjectAndStatus(subjectOpenid string, status int) ([]*Friend, error) {
+	objectList := make([]*Friend, 0)
+	err := this.db.Model(&Friend{}).Where("subject_openid = ? AND status = ?", subjectOpenid, status).Find(&objectList).Error
+	return objectList, err
+}
+
 // UpdateStatusByOSbject function
 func (this *DaoFriend) UpdateStatusByOSbject(objectOpenid, subjectOpenid string, stauts int) error {
 	err := this.db.Model(&Friend{}).Where("object_openid = ? AND subject_openid = ?", objectOpenid, subjectOpenid).Update("status", stauts).Error
