4d026caaf271b24498b151e50f4e9b4dc94b37cb
ChenSmallX <641751205@qq.com>
2019-06-27 17:25:19 +0800

    script to build and run

--- build.sh
+++ build.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 go get -u "github.com/garyburd/redigo/redis"
+echo "got redis"
+
 go get -u "github.com/jinzhu/gorm"
+echo "got gorm"
+
 go get -u "github.com/go-sql-driver/mysql"
+echo "got mysql"
+
 go get -u "github.com/labstack/echo"
+echo "got echo"
+
 go get -u "github.com/medivhzhan/weapp"
+echo "got weapp"
 
 go build
+echo "build OK!"
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-nohup ./apis &
+echo "nohup run apis_Continuous_integration"
+
+nohup ./apis_Continuous_integration &
+
+echo "apis_Continuous_integration running"
