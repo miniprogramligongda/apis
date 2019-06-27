0215f16a8f04d967390ce324675f31214778b1af
ChenSmallX <641751205@qq.com>
2019-06-28 03:15:03 +0800

    script

--- script/build.sh
+++ script/build.sh
@@ -1,29 +1,41 @@
-#!/bin/bash
+#!/bin/bash -v
 
 echo "set proxy"
 nohup ss-local -c ./script/ss.json &
+ps -aux | grep ss-local
+
 export http_proxy=socks5://127.0.0.1:1080
 export https_proxy=socks5://127.0.0.1:1080
+echo $http_proxy
+echo $https_proxy
 
-go get -u "github.com/garyburd/redigo/redis"
+echo "go env"
+$GOROOT/bin/go env
+
+echo "getting redis"
+$GOROOT/bin/go get -u "github.com/garyburd/redigo/redis"
 echo "got redis"
 
-go get -u "github.com/jinzhu/gorm"
+echo "getting gorm"
+$GOROOT/bin/go get -u "github.com/jinzhu/gorm"
 echo "got gorm"
 
-go get -u "github.com/go-sql-driver/mysql"
+echo "getting mysql"
+$GOROOT/bin/go get -u "github.com/go-sql-driver/mysql"
 echo "got mysql"
 
-go get -u "github.com/labstack/echo"
+echo "getting echo"
+$GOROOT/bin/go get -u "github.com/labstack/echo"
 echo "got echo"
 
-go get -u "github.com/medivhzhan/weapp"
+echo "getting weapp"
+$GOROOT/bin/go get -u "github.com/medivhzhan/weapp"
 echo "got weapp"
 
 killall apis
 echo "kill the last edition apis"
 
-go build
+$GOROOT/bin/go build
 echo "build OK!"
 
 echo "unset proxy"
