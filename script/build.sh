a501b405590ecddbf3eb8f5452b4c4d2bc8a0f60
ChenSmallX <641751205@qq.com>
2019-06-28 03:38:08 +0800

    script

--- script/build.sh
+++ script/build.sh
@@ -10,32 +10,32 @@ echo $http_proxy
 echo $https_proxy
 
 echo "/usr/local/go/bin/go  env"
-/usr/local/go/bin/go  env
+go  env
 
 echo "getting redis"
-/usr/local/go/bin/go  get -u "github.com/garyburd/redigo/redis"
+go  get -u "github.com/garyburd/redigo/redis"
 echo "got redis"
 
 echo "getting gorm"
-/usr/local/go/bin/go  get -u "github.com/jinzhu/gorm"
+go  get -u "github.com/jinzhu/gorm"
 echo "got gorm"
 
 echo "getting mysql"
-/usr/local/go/bin/go  get -u "github.com/go-sql-driver/mysql"
+go  get -u "github.com/go-sql-driver/mysql"
 echo "got mysql"
 
 echo "getting echo"
-/usr/local/go/bin/go  get -u "github.com/labstack/echo"
+go  get -u "github.com/labstack/echo"
 echo "got echo"
 
 echo "getting weapp"
-/usr/local/go/bin/go  get -u "github.com/medivhzhan/weapp"
+go  get -u "github.com/medivhzhan/weapp"
 echo "got weapp"
 
 killall apis
 echo "kill the last edition apis"
 
-/usr/local/go/bin/go  build
+go  build
 echo "build OK!"
 
 echo "unset proxy"
